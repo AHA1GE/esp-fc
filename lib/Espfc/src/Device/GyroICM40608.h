f16f19b7e4006f2b7fde542507e0bd79283359ab
Harry Sun <syfsyf@me.com>
2025-02-21 08:50:25 +0800

    update

--- lib/Espfc/src/Device/GyroICM40608.h
+++ lib/Espfc/src/Device/GyroICM40608.h
@@ -116,12 +116,12 @@ namespace Espfc
           return 0;
         // https://github.com/Mert-Kilic/ICM-42688-P/blob/main/icm.c
         uint8_t configure_reset = 0x01;
-        uint8_t fifo_conf_data = 0x03;
-        uint8_t buffer = 0x1F; // temperature sensor enabled. RC oscillator is on, gyro and accelerometer low noise mode,
+        uint8_t pwr_mgmt_buffer = 0x1F; // temperature sensor enabled. RC oscillator is on, gyro and accelerometer low noise mode,
         uint8_t fifo_init = 0x40;
-        _bus->writeByte(_addr, 0x11, configure_reset); // DEVICE_CONFIG 0x11
+        uint8_t fifo_conf_data = 0x03;
         _bus->writeByte(_addr, ICM40608_RA_REG_BANK_SEL, ICM40608_BANK_SELECT0); // select bank 0
-        _bus->writeByte(_addr, ICM40608_RA_PWR_MGMT0, buffer);
+        _bus->writeByte(_addr, 0x11, configure_reset);                           // DEVICE_CONFIG 0x11
+        _bus->writeByte(_addr, ICM40608_RA_PWR_MGMT0, pwr_mgmt_buffer);
         delay(100);
         _bus->writeByte(_addr, 0x16, fifo_init);      // FIFO_CONFIG_INIT 0x16
         _bus->writeByte(_addr, 0x5F, fifo_conf_data); // FIFO_CONFIGURATION 0x5F
@@ -155,7 +155,6 @@ namespace Espfc
 
       void setDLPFMode(uint8_t mode) override
       {
-        
       }
 
       int getRate() const override
@@ -175,9 +174,7 @@ namespace Espfc
         return whoami == ICM40608_WHO_AM_I_CONST;
       }
     };
-
   }
-
 }
 
 #endif
